6247be3f993bea88cfda67e1a3f2f12c0340c95f
Cuh4a <82233194+Cuh4a@users.noreply.github.com>
2022-02-21 09:57:40 +0300

    update js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let open = [];
+  let brackets = {};
+  bracketsConfig.map((a) => open.push(a[0]));
+  bracketsConfig.forEach((a) => (brackets[a[1]] = a[0]));
+  console.log(open);
+  console.log(brackets);
+  let a = [];
+  for (let i = 0; i < str.length; i++) {
+    let cur = str[i];
+    if (open.includes(cur)) {
+      if (
+        Object.keys(brackets).includes(cur) &&
+        a[a.length - 1] === cur
+      ) {
+        a.pop();
+      } else {
+        a.push(cur);
+      }
+    } else {
+      if (a.length === 0) {
+        return false;
+      }
+      let item = a[a.length - 1];
+      if (brackets[cur] === item) {
+        a.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return a.length === 0;;
+};
+
